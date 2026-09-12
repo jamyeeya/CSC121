5663d9a592b7666c8a853d028261fcfd1bd795f3
Jamyee Alston <jybaker965@access.alamancecc.edu>
2026-09-12 08:27:37 -0700

    week 3 assignment book_tracker

---
+++ book_tracker.py
@@ -0,0 +1,35 @@
+def dashboard():
+        print(40 , '=')
+        print("my library")
+
+    
+def estimate_reading_time(pages):
+        print("pages per hour = 40")
+        return round(pages_per_hour , 1)
+
+
+def add_book():
+        title = input("the long way to a small, angry planet"), 
+        author = input("Becky Chambers")
+        pages = input("438")
+        hours = input("10.9 hours")
+        print(f"{title} by {author} --approx.{hours} to read")
+
+
+def main():
+    dashboard()
+    add_book()
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
+    
+        
+
+
+
+
